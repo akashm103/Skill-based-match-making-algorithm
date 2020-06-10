0e0a8cb33c144876559e1b287dd86b523d3ef156
akashm103 <38016243+akashm103@users.noreply.github.com>
2020-06-10 17:37:00 +0530

    Update SoStronk.cpp

--- SoStronk.cpp
+++ SoStronk.cpp
@@ -1,3 +1,9 @@
+// the approach for the code is:
+//     firstly the permutation of every players is taken in and grouped into m members contaning a team such that there is no repeated team
+//     and it is saved in acoording to their index in vector 'ans'
+//     then a vector containg the index of 'ans' in 'superans' with their average 
+//     In Last it is printed according to differnce of average of both teams
+// Major use of hashing to make understanding easy and backtraking for the permutations
 #include<bits/stdc++.h>
 using namespace std;
 vector<vector<int>>ans;
@@ -29,6 +35,7 @@ main()
     #endif
     int m,n;
     cin>>m;
+    //m is the number of teams on one side
     int i,j,k;
     int a,b;
     string s,s1;
@@ -36,6 +43,9 @@ main()
     i=0;
     cin.ignore();
     getline(cin,s);
+//     it is not defined that how many total players will be and it is given that 
+//     to break taking the input after a space is given
+//     the input is taken in a single and then sperated into strings and integers
     while(!s.empty())
     {
         string xx="";
@@ -49,9 +59,11 @@ main()
         for(j=i+1;j<s.size();j++){
             val=val*10+(s[j]-'0');
         }
+//       loop to seprate the integer from string
         v.push_back({xx,val});
         getline(cin,s);
     }
+    
     for(i=0;i<v.size();i++)
     {
         // cout<<v[i].first<<" ";
@@ -59,7 +71,10 @@ main()
         vs.push_back(i);
         solve(i,vs,v.size(),m,1);
     }
+//     all the permutation of the players into a grouping of m is saved in 'ans' that is declared above as a global variable
+//     in 'ans' all the players are saved according to their index
     vector<pair<pair<int,int>,float>>superans;
+    //'superans' is a vector contaning the index of index of 'ans' and average based on the permutation
     for(i=0;i<ans.size();i++){
         unordered_map<int,int>mm;
         float avg=0;
@@ -84,6 +99,7 @@ main()
             }
         }
     }
+//     the above loop is saving the index and average in 'superans'
     sort(superans.begin(),superans.end(),cmp);
     for(auto x:superans){
         float f=0;
@@ -103,3 +119,4 @@ main()
         cout<<endl;
     }
 }
+//above loop prints the matchmaking of players in such a way that the difference of the average of both team is in descending order
